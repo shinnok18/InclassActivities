98c1c951f23eb09bfdf2e31bfef7d900440d7727
Orçun Türkokuloğlu <34009930+shinnok18@users.noreply.github.com>
2023-06-06 16:20:35 +0300

    Update Turkokuloglu-Activity12.js

--- Turkokuloglu-InClassActivity11/Turkokuloglu-Activity12.js
+++ Turkokuloglu-InClassActivity11/Turkokuloglu-Activity12.js
@@ -1,53 +1,32 @@
-// Get the main element
-var mainElement = document.querySelector('main');
+$(document).ready(function() {
+	// Click event handler for sidebar links
+	$("#nav_list a").click(function(e) {
+		e.preventDefault(); // Prevent default link behavior
 
-// Get the links in the sidebar
-var sidebarLinks = document.querySelectorAll('#nav_list a');
+		// Clear the main element
+		$("main").empty();
 
-// Add click event handlers to the links
-for (var i = 0; i < sidebarLinks.length; i++) {
-  sidebarLinks[i].addEventListener('click', function(event) {
-    // Prevent the default link behavior
-    event.preventDefault();
+		// Get the JSON file name from the clicked link's title attribute
+		var jsonFile = $(this).attr("title") + ".json";
 
-    // Get the JSON file name from the link's title attribute
-    var fileName = this.getAttribute('title') + '.json';
+		// Ajax request to load JSON data
+		$.ajax({
+			url: "json_files/" + jsonFile,
+			dataType: "json",
+			success: function(data) {
+				// Create HTML elements for the speaker data
+				var speakerContainer = $("<div>").addClass("speaker");
+				var speakerImage = $("<img>").attr("src", "images/" + data.image);
+				var speakerName = $("<h2>").text(data.name);
+				var speakerBio = $("<p>").text(data.bio);
 
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-
-    // Set up the AJAX request
-    xhr.open('GET', 'json_files/' + fileName, true);
-
-    // Set the response type to JSON
-    xhr.responseType = 'json';
-
-    // Set up the onload event handler
-    xhr.onload = function() {
-      // Check if the request was successful
-      if (xhr.status === 200) {
-        // Clear the main element
-        mainElement.innerHTML = '';
-
-        // Get the JSON data
-        var data = xhr.response;
-
-        // Create HTML elements for the speaker's information
-        var imgElement = document.createElement('img');
-        imgElement.src = 'images/' + data.image;
-        mainElement.appendChild(imgElement);
-
-        var h2Element = document.createElement('h2');
-        h2Element.innerHTML = data.month + '<br>' + data.name;
-        mainElement.appendChild(h2Element);
-
-        var pElement = document.createElement('p');
-        pElement.textContent = data.description;
-        mainElement.appendChild(pElement);
-      }
-    };
-
-    // Send the AJAX request
-    xhr.send();
-  });
-}
+				// Append the elements to the main element
+				speakerContainer.append(speakerImage, speakerName, speakerBio);
+				$("main").append(speakerContainer);
+			},
+			error: function() {
+				$("main").text("Error loading speaker data.");
+			}
+		});
+	});
+});

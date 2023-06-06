9149921e8302396bf6133b07c89ab21ae335b31b
Orçun Türkokuloğlu <34009930+shinnok18@users.noreply.github.com>
2023-06-06 16:14:12 +0300

    Update Turkokuloglu-Activity12.js

--- Turkokuloglu-InClassActivity11/Turkokuloglu-Activity12.js
+++ Turkokuloglu-InClassActivity11/Turkokuloglu-Activity12.js
@@ -1,24 +1,53 @@
-$(document).ready(function() {
-	// Click event handlers for the sidebar links
-	$("#nav_list a").click(function(e) {
-		e.preventDefault();
-		
-		// Get the JSON file name from the clicked link's title attribute
-		var jsonFileName = $(this).attr("title") + ".json";
-		
-		// Clear the elements from the main element
-		$("#speakerName").empty();
-		$("#speakerImage").attr("src", "");
-		$("#speakerDate").empty();
-		$("#speakerBio").empty();
-		
-		// Make an Ajax request to get the data from the JSON file
-		$.getJSON("json_files/" + jsonFileName, function(data) {
-			// Populate the HTML elements with the retrieved data
-			$("#speakerName").text(data.name);
-			$("#speakerImage").attr("src", "images/" + data.image);
-			$("#speakerDate").text(data.date);
-			$("#speakerBio").text(data.bio);
-		});
-	});
-});
+// Get the main element
+var mainElement = document.querySelector('main');
+
+// Get the links in the sidebar
+var sidebarLinks = document.querySelectorAll('#nav_list a');
+
+// Add click event handlers to the links
+for (var i = 0; i < sidebarLinks.length; i++) {
+  sidebarLinks[i].addEventListener('click', function(event) {
+    // Prevent the default link behavior
+    event.preventDefault();
+
+    // Get the JSON file name from the link's title attribute
+    var fileName = this.getAttribute('title') + '.json';
+
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+
+    // Set up the AJAX request
+    xhr.open('GET', 'json_files/' + fileName, true);
+
+    // Set the response type to JSON
+    xhr.responseType = 'json';
+
+    // Set up the onload event handler
+    xhr.onload = function() {
+      // Check if the request was successful
+      if (xhr.status === 200) {
+        // Clear the main element
+        mainElement.innerHTML = '';
+
+        // Get the JSON data
+        var data = xhr.response;
+
+        // Create HTML elements for the speaker's information
+        var imgElement = document.createElement('img');
+        imgElement.src = 'json_files/' + data.image;
+        mainElement.appendChild(imgElement);
+
+        var h2Element = document.createElement('h2');
+        h2Element.textContent = data.month + '\n' + data.name;
+        mainElement.appendChild(h2Element);
+
+        var pElement = document.createElement('p');
+        pElement.textContent = data.description;
+        mainElement.appendChild(pElement);
+      }
+    };
+
+    // Send the AJAX request
+    xhr.send();
+  });
+}

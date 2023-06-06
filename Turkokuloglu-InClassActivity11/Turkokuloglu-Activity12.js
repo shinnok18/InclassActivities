601d0a7793f9e0ac5f79c832f854800fea36a680
Orçun Türkokuloğlu <34009930+shinnok18@users.noreply.github.com>
2023-06-06 16:32:01 +0300

    Update Turkokuloglu-Activity12.js

--- Turkokuloglu-InClassActivity11/Turkokuloglu-Activity12.js
+++ Turkokuloglu-InClassActivity11/Turkokuloglu-Activity12.js
@@ -1,32 +1,25 @@
 $(document).ready(function() {
-	// Click event handler for sidebar links
-	$("#nav_list a").click(function(e) {
-		e.preventDefault(); // Prevent default link behavior
+  
+    $("#nav_list a").click(function(e) {
+      var title = $(this).attr("title");
+      SpeakerData(title);
+    });
 
-		// Clear the main element
-		$("main").empty();
-
-		// Get the JSON file name from the clicked link's title attribute
-		var jsonFile = $(this).attr("title") + ".json";
-
-		// Ajax request to load JSON data
-		$.ajax({
-			url: "json_files/" + jsonFile,
-			dataType: "json",
-			success: function(data) {
-				// Create HTML elements for the speaker data
-				var speakerContainer = $("<div>").addClass("speaker");
-				var speakerImage = $("<img>").attr("src", "images/" + data.image);
-				var speakerName = $("<h2>").text(data.name);
-				var speakerBio = $("<p>").text(data.bio);
-
-				// Append the elements to the main element
-				speakerContainer.append(speakerImage, speakerName, speakerBio);
-				$("main").append(speakerContainer);
-			},
-			error: function() {
-				$("main").text("Error loading speaker data.");
-			}
-		});
-	});
-});
+    function SpeakerData(file) {
+      var fileName = file + '.json';
+      $.getJSON(fileName, function(data) {
+        
+        $("main").empty();  //clear the elements
+  
+        data.speakers.forEach(function(speaker) {
+          var title = $("<h1>").text(speaker.title);
+          var image = $("<img>").attr("src", speaker.image);
+          var month = $("<h2>").html(speaker.month + "<br>" + speaker.speaker);
+          var description = $("<p>").text(speaker.text);
+  
+          $("main").append(title, image, month, description);
+        });
+      });
+    }
+  
+  });
